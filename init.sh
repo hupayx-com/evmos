dcdabb2e6d2543ea11b0b2603123d82246ebfb28
eunjay lee <ej.lee@hupayx.com>
2022-01-18 14:29:39 +0900

    sh test

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ TRACE=""
 command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
 
 # remove existing daemon
-rm -rf ~/.evmosd*
+rm -rf ~/.hupayx*
 
 # make install
 
@@ -27,45 +27,50 @@ evmosd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO
 evmosd init $MONIKER --chain-id $CHAINID 
 
 # Change parameter token denominations to aphoton
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="ahpx"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="ahpx"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="ahpx"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="ahpx"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $HOME/.hupayx/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="ahpx"' > $HOME/.hupayx/config/tmp_genesis.json && mv $HOME/.hupayx/config/tmp_genesis.json $HOME/.hupayx/config/genesis.json
+cat $HOME/.hupayx/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="ahpx"' > $HOME/.hupayx/config/tmp_genesis.json && mv $HOME/.hupayx/config/tmp_genesis.json $HOME/.hupayx/config/genesis.json
+cat $HOME/.hupayx/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="ahpx"' > $HOME/.hupayx/config/tmp_genesis.json && mv $HOME/.hupayx/config/tmp_genesis.json $HOME/.hupayx/config/genesis.json
+cat $HOME/.hupayx/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="ahpx"' > $HOME/.hupayx/config/tmp_genesis.json && mv $HOME/.hupayx/config/tmp_genesis.json $HOME/.hupayx/config/genesis.json
+
+
+cat $HOME/.hupayx/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="ahpx"' > $HOME/.hupayx/config/tmp_genesis.json && mv $HOME/.hupayx/config/tmp_genesis.json $HOME/.hupayx/config/genesis.json
+cat $HOME/.hupayx/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="ahpx"' > $HOME/.hupayx/config/tmp_genesis.json && mv $HOME/.hupayx/config/tmp_genesis.json $HOME/.hupayx/config/genesis.json
+
 
 # increase block time (?)
-cat $HOME/.evmosd/config/genesis.json | jq '.consensus_params["block"]["time_iota_ms"]="30000"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $HOME/.hupayx/config/genesis.json | jq '.consensus_params["block"]["time_iota_ms"]="30000"' > $HOME/.hupayx/config/tmp_genesis.json && mv $HOME/.hupayx/config/tmp_genesis.json $HOME/.hupayx/config/genesis.json
 
 # Set gas limit in genesis
-cat $HOME/.evmosd/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="10000000"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $HOME/.hupayx/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="10000000"' > $HOME/.hupayx/config/tmp_genesis.json && mv $HOME/.hupayx/config/tmp_genesis.json $HOME/.hupayx/config/genesis.json
 
 # disable produce empty block
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOME/.evmosd/config/config.toml
+    sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOME/.hupayx/config/config.toml
   else
-    sed -i 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOME/.evmosd/config/config.toml
+    sed -i 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOME/.hupayx/config/config.toml
 fi
 
 if [[ $1 == "pending" ]]; then
   if [[ "$OSTYPE" == "darwin"* ]]; then
-      sed -i '' 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.evmosd/config/config.toml
+      sed -i '' 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.hupayx/config/config.toml
+      sed -i '' 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.hupayx/config/config.toml
+      sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.hupayx/config/config.toml
+      sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.hupayx/config/config.toml
+      sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.hupayx/config/config.toml
+      sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.hupayx/config/config.toml
+      sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.hupayx/config/config.toml
+      sed -i '' 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.hupayx/config/config.toml
+      sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.hupayx/config/config.toml
   else
-      sed -i 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.evmosd/config/config.toml
+      sed -i 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.hupayx/config/config.toml
+      sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.hupayx/config/config.toml
+      sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.hupayx/config/config.toml
+      sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.hupayx/config/config.toml
+      sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.hupayx/config/config.toml
+      sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.hupayx/config/config.toml
+      sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.hupayx/config/config.toml
+      sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.hupayx/config/config.toml
+      sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.hupayx/config/config.toml
   fi
 fi
 
