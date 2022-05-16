c9be0fd44996d870c7126ad50c7de5e881dd7c48
eunjay lee <ej.lee@hupayx.com>
2022-05-16 18:31:38 +0900

    multicoinsend module add

--- app/upgrades/v4/constants.go
+++ app/upgrades/v4/constants.go
@@ -4,9 +4,9 @@ const (
 	// UpgradeName is the shared upgrade plan name for mainnet and testnet
 	UpgradeName = "v4.0.0"
 	// MainnetUpgradeHeight defines the Evmos mainnet block height on which the upgrade will take place
-	MainnetUpgradeHeight = 257_850
+	MainnetUpgradeHeight = 1
 	// TestnetUpgradeHeight defines the Evmos testnet block height on which the upgrade will take place
-	TestnetUpgradeHeight = 1_200_000
+	TestnetUpgradeHeight = 1
 	// UpgradeInfo defines the binaries that will be used for the upgrade
 	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/evmos/evmos/releases/download/v4.0.0/evmos_4.0.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v4.0.0/evmos_4.0.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v4.0.0/evmos_4.0.0_Linux_arm64.tar.gz","linux/x86_64":"https://github.com/evmos/evmos/releases/download/v4.0.0/evmos_4.0.0_Linux_x86_64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v4.0.0/evmos_4.0.0_Windows_x86_64.zip"}}'`
 
